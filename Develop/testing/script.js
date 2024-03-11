9d4b82a054d000024ffc9cadee5c2dc246b231b8
Jake R <jakerasmusson@gmail.com>
2024-03-11 14:16:34 -0500

    added display arrary

--- Develop/testing/script.js
+++ Develop/testing/script.js
@@ -1,14 +1,14 @@
-addEmployee.onclick =  function() {
+addEmployee.onclick = function () {
     const employeesArray = [];
     let whileloop = true;
-    while (whileloop == true){
+    while (whileloop == true) {
         let firstName = prompt('Employee First name');
         let lastName = prompt('Employee Last name');
         let salary = prompt('Salary');
         if (isNaN(salary)) {
             salary = 0;
         }
-        employeesArray.push({'First Name':firstName,'Last Name':lastName,'Salary':salary});
+        employeesArray.push({ 'First Name': firstName, 'Last Name': lastName, 'Salary': salary });
 
         let userContinue = confirm("Would you like to continue?");
 
@@ -25,5 +25,11 @@ addEmployee.onclick =  function() {
 
 
 function displayAverageSalary(employeesArray) {
-    console.log(employeesArray)
+    let total = 0;
+    let count = 0;
+    employeesArray.forEach(employee => {
+        total += employeesArray['Salary']
+        count++
+    });
+    console.log(total / count)
 }
